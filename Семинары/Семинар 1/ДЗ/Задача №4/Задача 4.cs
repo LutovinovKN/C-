11a3ef6676556bd69c86dd1958b33b4893942da3
Lutovinov Konstantin <Vesperlkn@mail.ru>
2022-08-20 19:47:40 +0300

    Добавлен gitignore

--- Семинары/Семинар 1/ДЗ/Задача №4/Задача 4.cs
+++ Семинары/Семинар 1/ДЗ/Задача №4/Задача 4.cs
@@ -6,15 +6,12 @@ Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
-if (number1 > number2 > number3)
+
+int Max(int number1, int number2, int number3)
 {
-    Console.WriteLine($"Max = {number1}");
+    int result = number1;
+    if (number2 > result) result = number2;
+    if (number3 > result) result = number3;
+    return result;
 }
-else if (number2 > number1 > number3)
-{
-    Console.WriteLine($"Max = {number2}");
-}
-else
-{
-    Console.WriteLine($"Max = {number3}");
-}
+Console.WriteLine(result);
