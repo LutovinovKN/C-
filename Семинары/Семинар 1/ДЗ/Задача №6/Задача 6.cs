2de855d5c3bc103624f126ff13ee50088a1dd49b
Lutovinov Konstantin <Vesperlkn@mail.ru>
2022-08-20 20:27:48 +0300

    Оформил и решил задачу №6

--- Семинары/Семинар 1/ДЗ/Задача №6/Задача 6.cs
+++ Семинары/Семинар 1/ДЗ/Задача №6/Задача 6.cs
@@ -2,3 +2,11 @@
 
 Console.Write("Введите число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0)
+{
+    Console.WriteLine($"Да");
+}
+else
+{
+    Console.WriteLine($"Нет");
+}
