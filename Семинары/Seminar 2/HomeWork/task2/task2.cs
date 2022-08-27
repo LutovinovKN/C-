1a980c726b2dd4214731e7a33dbfeb0a39138e50
Lutovinov Konstantin <Vesperlkn@mail.ru>
2022-08-27 11:42:31 +0300

    Исправил в Семинаре 2 Задачу №2

--- Семинары/Seminar 2/HomeWork/task2/task2.cs
+++ Семинары/Seminar 2/HomeWork/task2/task2.cs
@@ -1,17 +1,20 @@
 ﻿// Задача №2
-
+Console.ForegroundColor = ConsoleColor.Green;
 int value = new Random().Next(1, 65537);
 
 //int value = 78;
 
-Console.WriteLine($"Случайное число = {value}");
-int digit_third = (value % 1000) / 100; // нахождение 3 цифры
+int digitCount = (int)Math.Log10(value) + 1;
 
+Console.WriteLine(digitCount);
+
+Console.WriteLine($"Случайное число = {value}");
 if (value < 100)
 {
     Console.WriteLine("Третьей цифры нет");
 }
 else
 {
+    int digit_third = (value % (Convert.ToInt32(Math.Pow(10, digitCount - 2)))) / (Convert.ToInt32(Math.Pow(10, digitCount - 3))); // нахождение 3 цифры
     Console.WriteLine($"Третьей цифрой из случайного числа является цифра {digit_third}");
 }
