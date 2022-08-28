b8b1dfdd6601ac11a67fdf2817a3522bb6116756
Lutovinov Konstantin <Vesperlkn@mail.ru>
2022-08-29 00:42:27 +0300

    Выполнил 1 здачу

---
+++ Семинары/Seminar 3/HomeWork/task1/task1.cs
@@ -0,0 +1,29 @@
+﻿// Задача №1 (Не важно, сколько будет цифр в числе, код покажет, полиндром это или нет)
+
+// int value = new Random().Next(10000, 100000); // Рандомное 5-ти значное число
+// Console.WriteLine($"Случайное число = {value}");
+
+
+int value = 12321; // Проверка полиндромом
+
+int digitCount = (int)Math.Log10(value) + 1;
+
+Console.WriteLine(digitCount); // Показывает из скольки цифр состоит число
+
+
+
+for (int i = 1; i < ((digitCount / 2) + 1); i++)
+{
+    if (((value % (Convert.ToInt32(Math.Pow(10, digitCount))) / (Convert.ToInt32(Math.Pow(10, digitCount - 1))))) == (value % (Convert.ToInt32(Math.Pow(10, i) / (Convert.ToInt32(Math.Pow(10, i - 1)))))))
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Полиндром");
+    }
+
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Не полиндром");
+    }
+}
+
