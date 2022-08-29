038d478a020e505dd7c68f91684ae43e6c1aeabc
Lutovinov Konstantin <Vesperlkn@mail.ru>
2022-08-29 12:01:33 +0300

    Исправил задачу №1

--- Семинары/Seminar 3/HomeWork/task1/task1.cs
+++ Семинары/Seminar 3/HomeWork/task1/task1.cs
@@ -1,24 +1,40 @@
 ﻿// Задача №1 (Не важно, сколько будет цифр в числе, код покажет, полиндром это или нет)
 
 int value = new Random().Next(10000, 100000); // Рандомное 5-ти значное число
+//int value = 12321; // Проверка полиндромом
 Console.WriteLine($"Случайное число = {value}");
 
-//int value = 12321; // Проверка полиндромом
+
+
+int a = 1;
+
 
 int digitCount = (int)Math.Log10(value) + 1;
 Console.WriteLine(digitCount); // Показывает из скольки цифр состоит число
 
 for (int i = 1; i < ((digitCount / 2) + 1); i++)
 {
-    if (((value % (Convert.ToInt32(Math.Pow(10, digitCount))) / (Convert.ToInt32(Math.Pow(10, digitCount - 1))))) == (value % (Convert.ToInt32(Math.Pow(10, i) / (Convert.ToInt32(Math.Pow(10, i - 1)))))))
+    if (((value % (Convert.ToInt32(Math.Pow(10, digitCount))) /
+    (Convert.ToInt32(Math.Pow(10, digitCount - 1))))) ==
+    (value % (Convert.ToInt32(Math.Pow(10, i) /
+    (Convert.ToInt32(Math.Pow(10, i - 1)))))))
     {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Полиндром");
+        a = 1;
     }
     else
     {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Не полиндром");
+        a = 0;
     }
+}
+
+if (a == 1)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Полиндром");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Не полиндром");
 }
 
