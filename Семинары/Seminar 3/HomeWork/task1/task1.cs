8dc098703ee4c738a3690a998f93b84feb4aa2d9
Lutovinov Konstantin <Vesperlkn@mail.ru>
2022-08-29 22:58:35 +0300

    Исправил ошибку в 1 задании 3 семинара

--- Семинары/Seminar 3/HomeWork/task1/task1.cs
+++ Семинары/Seminar 3/HomeWork/task1/task1.cs
@@ -1,30 +1,37 @@
 ﻿// Задача №1 (Не важно, сколько будет цифр в числе, код покажет, полиндром это или нет)
 
-int value = new Random().Next(10000, 100000); // Рандомное 5-ти значное число
-//int value = 12321; // Проверка полиндромом
+//int value = new Random().Next(10000, 100000); // Рандомное 5-ти значное число
+int value = 12231; // Проверка полиндромом
 Console.WriteLine($"Случайное число = {value}");
 
-int a = 1;
+bool res = false;
 
 int digitCount = (int)Math.Log10(value) + 1;
 Console.WriteLine(digitCount); // Показывает из скольки цифр состоит число
-
-for (int i = 1; i < ((digitCount / 2) + 1); i++)
+//int i = 0;
+for (int i = 1; i <= (digitCount / 2); i++)
 {
-    if (((value % (Convert.ToInt32(Math.Pow(10, digitCount))) /
-    (Convert.ToInt32(Math.Pow(10, digitCount - 1))))) ==
-    (value % (Convert.ToInt32(Math.Pow(10, i) /
-    (Convert.ToInt32(Math.Pow(10, i - 1)))))))
+    int left = value % Convert.ToInt32(Math.Pow(10, digitCount - i + 1)) / Convert.ToInt32(Math.Pow(10, digitCount - i));
+
+    int right = value % Convert.ToInt32(Math.Pow(10, i)) / Convert.ToInt32(Math.Pow(10, i - 1));
+
+    if (left == right)  // Сравниваю крайнюю правую цифру с крайней левой цифрой и перемещаюсь на прорядок ближе к центру числа
+    // Сравниваю крайнюю левую цифру с крайней правой цифрой и перемещаюсь на прорядок ближе к центру числа
     {
-        a = 1;
+        Console.WriteLine(left);
+        Console.WriteLine(right);
+        res = true;
     }
     else
     {
-        a = 0;
+        Console.WriteLine(left);
+        Console.WriteLine(right);
+        res = false;
+        break;
     }
 }
 
-if (a == 1)
+if (res == true)
 {
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Полиндром");
