423222d29ca28fcadd6493e76f8e2b2f04889498
LutovinovKN <VesperLKN@mail.ru>
2022-09-01 12:48:48 +0300

    .

--- Семинары/Seminar 4/HomeWork/task2/task2.cs
+++ Семинары/Seminar 4/HomeWork/task2/task2.cs
@@ -6,23 +6,41 @@
 
 Console.Write("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());          //? ввод числа
+//int num = 452;
 int tmp = num;                                          //? копия числа с удаляемой последней цифрой
 int digit = 0;
 
 int digitCount = (int)Math.Log10(num) + 1;              //? нахождение количества цифр
 int[] divider = new int[digitCount];                    //? массив для делителей размером количества цифр
 
-for (int i = 0; i > 9; i++)
+Console.WriteLine(string.Join(",", divider));           //! потом удалить
+
+for (int i = 0; i < digitCount; i++)
 {
+    Console.WriteLine(string.Join(",", divider));       //! потом удалить
+
     digit = tmp % 10;                                   // берётся последняя цифра
     if (digit == 0)                                     // если цифра == 0, то перейти на следующую цифру, на 0 делить нельзя
     {
+        tmp /= 10;
         continue;
     }
-    else if (num / digit == 0)                          // если num / на последнюю цифру == 0
+    else if (tmp / digit == 0)                          // если num / на последнюю цифру == 0
     {
-        divider[digitCount] = digit;                    // то последнюю цифру внести в массив divider
+        divider[tmp] = digit;//! ЗДЕСЬ ОШИБКА
+        tmp /= 10;                     // то последнюю цифру внести в массив divider
     }
-    tmp /= 10;// удалить последнюю цифру
+    else
+    {
+        tmp /= 10;
+        continue;
+    }
+    tmp /= 10;                                          // удалить последнюю цифру
 }
-
+Console.WriteLine(string.Join(",", divider));           //! потом удалить
+int Sum = 0;
+for (int i = 0; i < digitCount; i++)
+{
+    Sum += divider[i];
+}
+Console.WriteLine("Сумма=" + Sum);
