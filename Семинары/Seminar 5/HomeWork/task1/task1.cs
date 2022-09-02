6b2b022fca73f1e43e3522b4e5ac61cc792c404a
LutovinovKN <VesperLKN@mail.ru>
2022-09-02 14:13:47 +0300

    Выполнил задание 1

---
+++ Семинары/Seminar 5/HomeWork/task1/task1.cs
@@ -0,0 +1,22 @@
+﻿// Задача №1
+
+//! Задать массив заполненный случайными положительными трехзначными  числами.
+//! найти сумму чётных элементов, стоящих на нечётных позициях
+
+Console.Write("Введите необходимое кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int sumnum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.WriteLine(array[i]);
+    if (array[i] % 2 == 0 && i % 2 != 0)
+    {
+        sumnum += 1;
+    }
+    Console.WriteLine(String.Join(",", array));
+}
+Console.WriteLine(sumnum);
+
