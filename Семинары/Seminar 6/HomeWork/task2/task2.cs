c8d5b946cdd8b8a3002519b29ada80de6cf3631d
LutovinovKN <VesperLKN@mail.ru>
2022-09-07 17:52:48 +0300

    Выполнил задачу №2

--- Семинары/Seminar 6/HomeWork/task2/task2.cs
+++ Семинары/Seminar 6/HomeWork/task2/task2.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №2
+
+//! Написать программу, которая найдёт точку пересеченияя двух прямых, заданных уравнениями.
+//! y = k1 * x + b1 | y = k2 * x + b2
+
+Console.WriteLine("Введите значение числа k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение числа b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение числа k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение числа b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+double x = 0;
+double y1 = 0;
+double y2 = 0;
+x = (b2 - b1) / (k1 - k2);
+Console.WriteLine($"X = {x}");
+y1 = k1 * x + b1;
+y2 = k2 * x + b2;
+Console.WriteLine($"({y1};{y1})");
+
+if ((k1 * b2 - k2 * b1) == 0)
+{
+    Console.WriteLine("Прямые параллельны другу друг и не имеют точек пересечения");
+}
+else if ((k1 * k2 + b1 * b2) == 0)
+{
+    Console.WriteLine("Прямые перпендикулярны");
+}
+
