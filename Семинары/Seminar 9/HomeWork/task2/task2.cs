b452dd19c7905155ed1a5b78ee193ca8620ccf3f
LutovinovKN <VesperLKN@mail.ru>
2022-09-20 22:13:03 +0300

    Выполнил 2 задачу

--- Семинары/Seminar 9/HomeWork/task2/task2.cs
+++ Семинары/Seminar 9/HomeWork/task2/task2.cs
@@ -7,77 +7,49 @@
 //!     11 16 15 06
 //!     10 09 08 07
 
-// Console.Clear();
+using System;
 
-// const int n = 4;
-// const int m = 4;
-// int[,] matrix = new int[n, m];
-
-// int row = 0;
-// int col = 0;
-// int dx = 1;
-// int dy = 0;
-// int dirChanges = 0;
-// int visits = m;
-
-// for (int i = 0; i < matrix.Length; i++)
-// {
-//     matrix[row, col] = i + 1;
-//     if (--visits == 0)
-//     {
-//         visits = m * (dirChanges % 2) + n * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
-//         int temp = dx;
-//         dx = -dy;
-//         dy = temp;
-//         dirChanges++;
-//     }
-
-//     col += dx;
-//     row += dy;
-//     Console.Write("{0,2}", matrix[row, col]);
-// }
-
-
-
-
-int main()
+class Spiral
 {
-    int a = int Next[4, 4];
-    int N, M;
-
-    scanf("%d%d", &N, &M);
-    
-    int Ibeg = 0, Ifin = 0, Jbeg = 0, Jfin = 0;
-    
-    int k = 1;
-    int i = 0;
-    int j = 0;
-
-    while (k <= N * M){
-        a[i][j] = k;
-        if (i == Ibeg && j < M - Jfin - 1)
-            ++j;
-        else if (j == M - Jfin - 1 && i < N - Ifin - 1)
-            ++i;
-        else if (i == N - Ifin - 1 && j > Jbeg)
-            --j;
-        else
-            --i;
-
-        if ((i == Ibeg + 1) && (j == Jbeg) && (Jbeg != M - Jfin - 1)){
-            ++Ibeg;
-            ++Ifin;
-            ++Jbeg;
-            ++Jfin;
+    static void DisplayArray(int[,] a)
+    {
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++) Console.Write("{0,3} ", a[i, j]);
+            Console.WriteLine();
         }
-        ++k;
-    }
-    
-    for (int i = 0; i < 10; ++i){
-        for (int j = 0; j < 10; ++j)
-            printf("%3d", a[i][j]);
-        printf("\n");
     }
 
-    return 0;
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Введите размерность массива (0 - exit): ");
+
+            int n;
+            if (!Int32.TryParse(Console.ReadLine(), out n) || n <= 0) break;
+
+            Console.WriteLine();
+
+            int[,] a = new int[n, n];
+
+            int i = 0, j = 0;
+
+            int value = 1;
+
+            while (n != 0)
+            {
+                int k = 0;
+                do { a[i, j++] = value++; } while (++k < n - 1);
+                for (k = 0; k < n - 1; k++) a[i++, j] = value++;
+                for (k = 0; k < n - 1; k++) a[i, j--] = value++;
+                for (k = 0; k < n - 1; k++) a[i--, j] = value++;
+
+                ++i; ++j; n = n < 2 ? 0 : n - 2;
+            }
+
+            DisplayArray(a);
+            Console.WriteLine();
+        }
+    }
 }
