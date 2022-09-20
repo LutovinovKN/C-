499c9a25c87a05227237995f0767440f66383bc9
LutovinovKN <VesperLKN@mail.ru>
2022-09-20 15:28:24 +0300

    Выполнил 1 задачу

--- Семинары/Seminar 9/HomeWork/task1/task1.cs
+++ Семинары/Seminar 9/HomeWork/task1/task1.cs
@@ -9,35 +9,35 @@ class Program
 {
     static void Main(String[] args)
     {
-        var a = new Int32[3, 4];
+        var arr = new Int32[3, 4];
 
         var random = new Random();
-        for (var i = 0; i < a.GetLength(0); i++, Console.WriteLine())
+        for (var i = 0; i < arr.GetLength(0); i++, Console.WriteLine())
         {
-            for (var j = 0; j < a.GetLength(1); j++)
+            for (var j = 0; j < arr.GetLength(1); j++)
             {
-                a[i, j] = random.Next(100);
-                Console.Write("{0,4}", a[i, j]);
+                arr[i, j] = random.Next(100);
+                Console.Write("{0,4}", arr[i, j]);
             }
         }
         Console.WriteLine("");
 
         //сортируем
-        for (var i = 0; i < a.GetLength(0); i++)
+        for (var i = 0; i < arr.GetLength(0); i++)
             //сортировка пузырьком
-            for (var j = 0; j < a.GetLength(1); j++)
-                for (var k = 0; k < a.GetLength(1); k++)
+            for (var j = 0; j < arr.GetLength(1); j++)
+                for (var k = 0; k < arr.GetLength(1); k++)
                 {
-                    if (a[i, j] <= a[i, k]) continue;
-                    var temp = a[i, j];
-                    a[i, j] = a[i, k];
-                    a[i, k] = temp;
+                    if (arr[i, j] <= arr[i, k]) continue;
+                    var temp = arr[i, j];
+                    arr[i, j] = arr[i, k];
+                    arr[i, k] = temp;
                 }
 
-        for (var i = 0; i < a.GetLength(0); i++, Console.WriteLine())
-            for (var j = 0; j < a.GetLength(1); j++)
+        for (var i = 0; i < arr.GetLength(0); i++, Console.WriteLine())
+            for (var j = 0; j < arr.GetLength(1); j++)
             {
-                Console.Write("{0,4}", a[i, j]);
+                Console.Write("{0,4}", arr[i, j]);
             }
     }
 }
